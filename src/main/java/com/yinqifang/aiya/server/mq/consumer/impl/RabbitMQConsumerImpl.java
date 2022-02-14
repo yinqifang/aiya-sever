61bed11df0607327cc774f1b2d2ae3a91aae5bf2
Yin Qifang <yiika@163.com>
2022-02-14 17:55:28 +0800

    minor update

--- src/main/java/com/yinqifang/aiya/server/mq/consumer/impl/RabbitMQConsumerImpl.java
+++ src/main/java/com/yinqifang/aiya/server/mq/consumer/impl/RabbitMQConsumerImpl.java
@@ -1,16 +1,13 @@
 package com.yinqifang.aiya.server.mq.consumer.impl;
 
-import com.yinqifang.aiya.server.config.RabbitMQConfig;
-import com.yinqifang.aiya.server.mq.consumer.MQConsumer;
 import com.yinqifang.aiya.server.mq.consumer.RabbitMQConsumer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Yin Qifang
@@ -30,17 +27,17 @@ public class RabbitMQConsumerImpl implements RabbitMQConsumer {
             log.info("No message find");
             return null;
         }
-        return new String(msg.getBody(), Charset.forName("utf-8"));
+        return new String(msg.getBody(), StandardCharsets.UTF_8);
     }
 
     //    @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME_AIYA)
-//    public void listen(String msg) {
-//        this.onMessage(msg);
-//    }
-//
-//    @Override
-//    public void onMessage(String msg) {
-//        log.info("Got message: {}", msg);
-//    }
+    //    public void listen(String msg) {
+    //        this.onMessage(msg);
+    //    }
+    //
+    //    @Override
+    //    public void onMessage(String msg) {
+    //        log.info("Got message: {}", msg);
+    //    }
 
 }
